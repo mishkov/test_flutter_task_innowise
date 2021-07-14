fba233925f9d1ed21164ba69f20ac867d74e6202
Никита Мишков <mischkowu@gmail.com>
2021-07-14 17:38:45 +0300

    created DayForecast class

---
+++ lib/day_forecast.dart
@@ -0,0 +1,8 @@
+import 'package:test_flutter_task_innowise/timed_temperature.dart';
+
+class DayWeather {
+  final String dayName;
+  final List<TimedTemperature> temperatures;
+
+  DayWeather(this.dayName, this.temperatures);
+}
