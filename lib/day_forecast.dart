180d7b6e47057397416ea6ce1f9afccfe838cbd2
Никита Мишков <mischkowu@gmail.com>
2021-07-15 10:51:22 +0300

    fixed incorrect name of field

--- lib/day_forecast.dart
+++ lib/day_forecast.dart
@@ -2,7 +2,7 @@ import 'package:test_flutter_task_innowise/timed_weather.dart';
 
 class DayForecast {
   final String dayName;
-  final List<TimedWeather> temperatures;
+  final List<TimedWeather> weathers;
 
-  DayForecast(this.dayName, this.temperatures);
+  DayForecast(this.dayName, this.weathers);
 }
