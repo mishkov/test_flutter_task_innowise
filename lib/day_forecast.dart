3dd88e1a43ace1baf355a378da68889135649f48
Никита Мишков <mischkowu@gmail.com>
2021-07-15 10:27:43 +0300

    replaced TimedTemperature with TimedWeather

--- lib/day_forecast.dart
+++ lib/day_forecast.dart
@@ -1,8 +1,8 @@
-import 'package:test_flutter_task_innowise/timed_temperature.dart';
+import 'package:test_flutter_task_innowise/timed_weather.dart';
 
 class DayForecast {
   final String dayName;
-  final List<TimedTemperature> temperatures;
+  final List<TimedWeather> temperatures;
 
   DayForecast(this.dayName, this.temperatures);
 }
