98fe4c293d301de699f38f6ad725dc48072bc3e5
Никита Мишков <mischkowu@gmail.com>
2021-07-15 18:28:38 +0300

    added "current weather" button

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:test_flutter_task_innowise/five_day_forecast_cubit.dart';
 import 'package:test_flutter_task_innowise/main_page_title_cubit.dart';
 
 import 'day_forecast.dart';
 import 'day_forecast_view.dart';
+import 'detail_weather_page.dart';
 import 'five_day_forecast_formatted_cubit.dart';
 
 void main() {
@@ -14,6 +15,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Weather',
       theme: ThemeData(
@@ -51,6 +56,19 @@ class MainPage extends StatelessWidget {
             );
           },
         ),
+      ),
+      floatingActionButton: ElevatedButton(
+        onPressed: () async {
+          final forecast = FiveDayForecastFormattedCubit();
+          final dayForecast = FiveDayForecastFormattedCubit().state.first;
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return DetailWeatherPage(dayForecast);
+            }),
+          );
+        },
+        child: Text('Current Weather'),
       ),
     );
   }
