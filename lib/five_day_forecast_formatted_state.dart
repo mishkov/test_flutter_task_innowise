ade855c6b1a4132b534ead4d9834d39943705cc0
Никита Мишков <mischkowu@gmail.com>
2021-07-16 13:25:48 +0300

    migrated to state that contains data and errors info

---
+++ lib/five_day_forecast_formatted_state.dart
@@ -0,0 +1,7 @@
+import 'package:test_flutter_task_innowise/cubit_state.dart';
+
+import 'day_forecast.dart';
+
+class FiveDayForecstFormattedState extends CubitState<List<DayForecast>> {
+  FiveDayForecstFormattedState() : super([]);
+}
---
+++ lib/main_page_title_state.dart
@@ -0,0 +1,5 @@
+import 'package:test_flutter_task_innowise/cubit_state.dart';
+
+class MainPageTitleState extends CubitState<String> {
+  MainPageTitleState() : super('Loading...');
+}
