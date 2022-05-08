406e25e4e97c04f66a327750c9c0c6f46de99ab8
Nikita Mishkov <mishkov.dd@gmail.com>
2022-05-08 19:36:36 +0300

    Move main page to external file

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:test_flutter_task_innowise/five_day_forecast/day_forecast.dart';
 
 import 'business_logic/states/cubit_state.dart';
 import 'main_page/app_bar/application_bar.dart';
+import 'main_page/main_page.dart';
 import 'ui/day_forecast_view.dart';
 import 'ui/detail_weather_page.dart';
 import 'business_logic/cubits/five_day_forecast_formatted_cubit.dart';
@@ -43,126 +44,6 @@ class MyApp extends StatelessWidget {
 
         return MaterialPageRoute(builder: (context) => MainPage());
       },
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  static final routeName = '/';
-
-  @override
-  Widget build(BuildContext mainPageContext) {
-    final fiveDayForecastFormattedCubit = FiveDayForecastFormattedCubit();
-
-    return Scaffold(
-      appBar: MainPageAppBar(),
-      body: Center(
-        child: BlocProvider.value(
-          value: fiveDayForecastFormattedCubit,
-          child: BlocBuilder<FiveDayForecastFormattedCubit,
-              FiveDayForecstFormattedState>(
-            bloc: fiveDayForecastFormattedCubit,
-            builder: (forecastContext, state) {
-              if (state.status == Status.done) {
-                return ListView.builder(
-                  physics: ClampingScrollPhysics(),
-                  itemCount: state.data.length,
-                  itemBuilder: (_, index) {
-                    return DayForecastView(state.data[index]);
-                  },
-                );
-              } else if (state.status == Status.loading) {
-                return const LoadingView();
-              } else if (state.status == Status.failed) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(state.errorDetail),
-                    const TryAgainButton(),
-                  ],
-                );
-              } else {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Unknown response status.'),
-                  ],
-                );
-              }
-            },
-          ),
-        ),
-      ),
-      floatingActionButton: BlocBuilder<FiveDayForecastFormattedCubit,
-          FiveDayForecstFormattedState>(
-        bloc: fiveDayForecastFormattedCubit,
-        builder: (_, state) {
-          if (state.status == Status.done) {
-            return ElevatedButton(
-              onPressed: () {
-                final dayForecast = state.data.first;
-                Navigator.pushNamed(
-                  mainPageContext,
-                  DetailWeatherPage.routeName,
-                  arguments: dayForecast,
-                );
-              },
-              child: Text('Current Weather'),
-            );
-          } else if (state.status == Status.loading) {
-            return ElevatedButton(
-              onPressed: null,
-              child: Text('Loading...'),
-            );
-          } else if (state.status == Status.failed) {
-            return ElevatedButton(
-              onPressed: null,
-              child: Text('Failed'),
-            );
-          } else {
-            return ElevatedButton(
-              onPressed: null,
-              child: Text('Unknown response status!'),
-            );
-          }
-        },
-      ),
-    );
-  }
-}
-
-class LoadingView extends StatelessWidget {
-  const LoadingView({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: EdgeInsets.only(top: 10, bottom: 10),
-          child: CircularProgressIndicator.adaptive(),
-        ),
-        Text(
-          'Loading...',
-        ),
-      ],
-    );
-  }
-}
-
-class TryAgainButton extends StatelessWidget {
-  const TryAgainButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        context.read<FiveDayForecastFormattedCubit>().tryAgain();
-      },
-      child: Text('Try again'),
     );
   }
 }
---
+++ lib/main_page/main_page.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../business_logic/cubits/five_day_forecast_formatted_cubit.dart';
+import '../business_logic/states/cubit_state.dart';
+import '../business_logic/states/five_day_forecast_formatted_state.dart';
+import '../ui/day_forecast_view.dart';
+import '../ui/detail_weather_page.dart';
+import 'app_bar/application_bar.dart';
+
+class MainPage extends StatelessWidget {
+  static final routeName = '/';
+
+  @override
+  Widget build(BuildContext mainPageContext) {
+    final fiveDayForecastFormattedCubit = FiveDayForecastFormattedCubit();
+
+    return Scaffold(
+      appBar: MainPageAppBar(),
+      body: Center(
+        child: BlocProvider.value(
+          value: fiveDayForecastFormattedCubit,
+          child: BlocBuilder<FiveDayForecastFormattedCubit,
+              FiveDayForecstFormattedState>(
+            bloc: fiveDayForecastFormattedCubit,
+            builder: (forecastContext, state) {
+              if (state.status == Status.done) {
+                return ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  itemCount: state.data.length,
+                  itemBuilder: (_, index) {
+                    return DayForecastView(state.data[index]);
+                  },
+                );
+              } else if (state.status == Status.loading) {
+                return const LoadingView();
+              } else if (state.status == Status.failed) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(state.errorDetail),
+                    const TryAgainButton(),
+                  ],
+                );
+              } else {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Unknown response status.'),
+                  ],
+                );
+              }
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: BlocBuilder<FiveDayForecastFormattedCubit,
+          FiveDayForecstFormattedState>(
+        bloc: fiveDayForecastFormattedCubit,
+        builder: (_, state) {
+          if (state.status == Status.done) {
+            return ElevatedButton(
+              onPressed: () {
+                final dayForecast = state.data.first;
+                Navigator.pushNamed(
+                  mainPageContext,
+                  DetailWeatherPage.routeName,
+                  arguments: dayForecast,
+                );
+              },
+              child: Text('Current Weather'),
+            );
+          } else if (state.status == Status.loading) {
+            return ElevatedButton(
+              onPressed: null,
+              child: Text('Loading...'),
+            );
+          } else if (state.status == Status.failed) {
+            return ElevatedButton(
+              onPressed: null,
+              child: Text('Failed'),
+            );
+          } else {
+            return ElevatedButton(
+              onPressed: null,
+              child: Text('Unknown response status!'),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          child: CircularProgressIndicator.adaptive(),
+        ),
+        Text(
+          'Loading...',
+        ),
+      ],
+    );
+  }
+}
+
+class TryAgainButton extends StatelessWidget {
+  const TryAgainButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        context.read<FiveDayForecastFormattedCubit>().tryAgain();
+      },
+      child: Text('Try again'),
+    );
+  }
+}
