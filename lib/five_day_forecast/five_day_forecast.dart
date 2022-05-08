4fa43dd257426a8d3773aa13d24676b1c8beeac6
Nikita Mishkov <mishkov.dd@gmail.com>
2022-05-08 19:58:03 +0300

    Add WeatherTimeoutException to stream

--- lib/five_day_forecast/five_day_forecast.dart
+++ lib/five_day_forecast/five_day_forecast.dart
@@ -33,6 +33,8 @@ class FiveDayForecast {
     final timedWeathers = await _weather
         .fiveDayForecastByLocation(_latitude, _longitude)
         .timeout(timeoutTime, onTimeout: () {
+      _forecastStreamController.addError(
+          WeatherTimeoutException(), StackTrace.current);
       throw WeatherTimeoutException();
     });
 
