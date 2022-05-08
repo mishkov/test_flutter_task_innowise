6aff257197f7b0d0dfcf4a11bcd0e948929a8770
Nikita Mishkov <mishkov.dd@gmail.com>
2022-05-08 16:52:15 +0300

    Add fromWeather constructor

--- lib/business_logic/models/timed_weather.dart
+++ lib/business_logic/models/timed_weather.dart
@@ -1,8 +1,18 @@
 import 'package:weather/weather.dart';
 
+extension _timeExtractor on Weather {
+  String get hoursAndMinutes {
+    final time = date != null ? '${date!.hour}:${date!.minute}' : '';
+
+    return time;
+  }
+}
+
 class TimedWeather {
   String time;
   Weather weather;
 
   TimedWeather(this.time, this.weather);
+
+  TimedWeather.fromWeather(this.weather) : time = weather.hoursAndMinutes;
 }
