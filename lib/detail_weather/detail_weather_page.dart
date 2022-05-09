b0048c921de23e3048a67bbf557f9aaf7e9ca94f
Nikita Mishkov <mishkov.dd@gmail.com>
2022-05-09 21:35:22 +0300

    Fix overflowed bug

--- lib/detail_weather/detail_weather_page.dart
+++ lib/detail_weather/detail_weather_page.dart
@@ -30,169 +30,172 @@ class DetailWeatherPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(getDayName(dayForecast.weekDay)),
       ),
-      body: Column(
-        children: [
-          Image.network(
-            iconUrl,
-            scale: 0.2,
-            loadingBuilder: (_, child, loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              }
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-            errorBuilder: (_, error, stackTrace) {
-              print('$error $stackTrace');
-              return Padding(
-                padding: EdgeInsets.only(top: 30, bottom: 10),
-                child: Icon(
-                  Icons.signal_wifi_connected_no_internet_4,
-                  color: Colors.grey[600],
-                  size: 60,
-                ),
-              );
-            },
-          ),
-          Text(
-            '${weather.areaName}, ${weather.country}',
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 20,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network(
+              iconUrl,
+              scale: 0.2,
+              loadingBuilder: (_, child, loadingProgress) {
+                if (loadingProgress == null) {
+                  return child;
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+              errorBuilder: (_, error, stackTrace) {
+                print('$error $stackTrace');
+                return Padding(
+                  padding: EdgeInsets.only(top: 30, bottom: 10),
+                  child: Icon(
+                    Icons.signal_wifi_connected_no_internet_4,
+                    color: Colors.grey[600],
+                    size: 60,
+                  ),
+                );
+              },
             ),
-          ),
-          Text(
-            '$temp°C | ${weather.weatherMain}',
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 30,
-            ),
-          ),
-          Divider(
-            height: 50,
-            indent: 100,
-            endIndent: 100,
-            thickness: 2,
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  weather.humidity != null
-                      ? Column(
-                          children: [
-                            Icon(
-                              WeatherIcons.humidity,
-                              size: iconSize,
-                              color: iconColor,
-                            ),
-                            SizedBox(
-                              height: iconSpace,
-                            ),
-                            Text(
-                              '${weather.humidity!.round()}%',
-                              style: iconTextStyle,
-                            ),
-                          ],
-                        )
-                      : SizedBox.shrink(),
-                  weather.rainLastHour != null
-                      ? Column(
-                          children: [
-                            Icon(
-                              WeatherIcons.raindrops,
-                              size: iconSize,
-                              color: iconColor,
-                            ),
-                            SizedBox(
-                              height: iconSpace,
-                            ),
-                            Text(
-                              '${weather.rainLastHour!.toStringAsFixed(1)} mm',
-                              style: iconTextStyle,
-                            ),
-                          ],
-                        )
-                      : SizedBox.shrink(),
-                  weather.pressure != null
-                      ? Column(
-                          children: [
-                            Icon(
-                              WeatherIcons.barometer,
-                              color: iconColor,
-                              size: iconSize,
-                            ),
-                            SizedBox(
-                              height: iconSpace,
-                            ),
-                            Text(
-                              '${weather.pressure} Pa',
-                              style: iconTextStyle,
-                            ),
-                          ],
-                        )
-                      : SizedBox.shrink(),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  weather.windSpeed != null
-                      ? Column(
-                          children: [
-                            Icon(
-                              WeatherIcons.strong_wind,
-                              size: iconSize,
-                              color: iconColor,
-                            ),
-                            SizedBox(
-                              height: iconSpace,
-                            ),
-                            Text(
-                              '${weather.windSpeed} m/s',
-                              style: iconTextStyle,
-                            ),
-                          ],
-                        )
-                      : SizedBox.shrink(),
-                  weather.windDegree != null
-                      ? Column(
-                          children: [
-                            WindIcon(
-                              degree: weather.windDegree!,
-                              size: iconSize,
-                              color: iconColor,
-                            ),
-                            Text(
-                              '${weather.windDegree} °',
-                              style: iconTextStyle,
-                            ),
-                          ],
-                        )
-                      : SizedBox.shrink(),
-                ],
-              ),
-            ],
-          ),
-          Divider(
-            height: 50,
-            indent: 100,
-            endIndent: 100,
-            thickness: 2,
-          ),
-          TextButton(
-            onPressed: _copyWeatherToCliBoard,
-            child: Text(
-              'Share',
+            Text(
+              '${weather.areaName}, ${weather.country}',
               style: TextStyle(
-                color: Colors.orange,
+                color: Colors.grey,
                 fontSize: 20,
               ),
             ),
-          ),
-        ],
+            Text(
+              '$temp°C | ${weather.weatherMain}',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+              ),
+            ),
+            Divider(
+              height: 50,
+              indent: 100,
+              endIndent: 100,
+              thickness: 2,
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    weather.humidity != null
+                        ? Column(
+                            children: [
+                              Icon(
+                                WeatherIcons.humidity,
+                                size: iconSize,
+                                color: iconColor,
+                              ),
+                              SizedBox(
+                                height: iconSpace,
+                              ),
+                              Text(
+                                '${weather.humidity!.round()}%',
+                                style: iconTextStyle,
+                              ),
+                            ],
+                          )
+                        : SizedBox.shrink(),
+                    weather.rainLastHour != null
+                        ? Column(
+                            children: [
+                              Icon(
+                                WeatherIcons.raindrops,
+                                size: iconSize,
+                                color: iconColor,
+                              ),
+                              SizedBox(
+                                height: iconSpace,
+                              ),
+                              Text(
+                                '${weather.rainLastHour!.toStringAsFixed(1)} mm',
+                                style: iconTextStyle,
+                              ),
+                            ],
+                          )
+                        : SizedBox.shrink(),
+                    weather.pressure != null
+                        ? Column(
+                            children: [
+                              Icon(
+                                WeatherIcons.barometer,
+                                color: iconColor,
+                                size: iconSize,
+                              ),
+                              SizedBox(
+                                height: iconSpace,
+                              ),
+                              Text(
+                                '${weather.pressure} Pa',
+                                style: iconTextStyle,
+                              ),
+                            ],
+                          )
+                        : SizedBox.shrink(),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    weather.windSpeed != null
+                        ? Column(
+                            children: [
+                              Icon(
+                                WeatherIcons.strong_wind,
+                                size: iconSize,
+                                color: iconColor,
+                              ),
+                              SizedBox(
+                                height: iconSpace,
+                              ),
+                              Text(
+                                '${weather.windSpeed} m/s',
+                                style: iconTextStyle,
+                              ),
+                            ],
+                          )
+                        : SizedBox.shrink(),
+                    weather.windDegree != null
+                        ? Column(
+                            children: [
+                              WindIcon(
+                                degree: weather.windDegree!,
+                                size: iconSize,
+                                color: iconColor,
+                              ),
+                              Text(
+                                '${weather.windDegree} °',
+                                style: iconTextStyle,
+                              ),
+                            ],
+                          )
+                        : SizedBox.shrink(),
+                  ],
+                ),
+              ],
+            ),
+            Divider(
+              height: 50,
+              indent: 100,
+              endIndent: 100,
+              thickness: 2,
+            ),
+            TextButton(
+              onPressed: _copyWeatherToCliBoard,
+              child: Text(
+                'Share',
+                style: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
